09157bae8dc5b28037f19954ff38d457376ae087
Pawel Bozemski <paw.boz@gmail.com>
2020-03-12 15:18:28 +0100

    Add handlebars to server.js and main layout

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname + `/views/${name}`));
-  };
-  next();
-});
-
-app.use('/user', (req, res, next) => {
- res.show('forbidden.html');
-});
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname + '/public')));
 
+app.use('/user', (req, res, next) => {
+  res.render('forbidden');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', {name: req.params.name });
+});
+
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/home', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
 });
 
 app.use((req, res) => {
-  res.status(404).show('404.html');
+  res.status(404).render('404');
 })
+
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
